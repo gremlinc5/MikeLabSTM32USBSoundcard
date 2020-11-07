428279ce3933c7587bce7fcec63d41d9ba647e09
gremlinc5 <chiesamichele84@gmail.com>
2020-11-07 21:57:32 +0100

    Added ST audio libraries

--- src/bsp/stm32f4_discovery_audio.h
+++ src/bsp/stm32f4_discovery_audio.h
@@ -49,7 +49,7 @@
 #include "cs43l22.h"
 
 #include "stm32f4_discovery.h"
-#include "../../../Middlewares/ST/STM32_Audio/Addons/PDM/pdm_filter.h"
+#include "../audio/STM32_Audio/Addons/PDM/pdm_filter.h"
 
 /** @addtogroup BSP
   * @{
